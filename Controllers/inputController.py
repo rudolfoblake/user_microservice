6f1a0f6088b923ac48a5ee9ba2291cbf59ad8193
Gustavo Lemos <gustavolemos7@hotmail.com>
2021-05-12 10:59:36 -0300

    Feat: Added inputController for verify user inputs.

---
+++ Controllers/inputController.py
@@ -0,0 +1,17 @@
+import json
+
+class InputControl:
+     def json_to_dict(self, request):
+          try:
+               req = request.get_json()
+          except:
+               return 'Error: Cannot convert data into json!', 400
+          return req, 200
+
+     def verify_user_register_requirements(self, req:dict) -> tuple:
+          try:
+               req['email']
+               req['password']
+          except:
+               return "Error: Requirements (email and password) no found in json", 400
+          return "Success", 200
