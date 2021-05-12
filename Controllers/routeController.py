0a7d32a43426ebe02bf4b643b8b5244c41060a50
Gustavo Lemos <gustavolemos7@hotmail.com>
2021-05-12 14:33:35 -0300

    Working on register route.

--- Controllers/routeController.py
+++ Controllers/routeController.py
@@ -9,7 +9,12 @@ class RouteControl:
     def register_route(self, req:dict) -> tuple:
         verify_user_register_requeriments = ic.verify_user_register_requirements(req)
         if verify_user_register_requeriments[1] != 200: return verify_user_register_requeriments
-        return req, 200
+        get_email = db.get_user_by_email(req["email"])
+        if get_email[1] == 200:
+            if len(get_email[0]) > 0:
+                return "Error: A user with that email already exists.", 400
+        else:
+            return get_email[0], get_email[1]
 
     def get_user_by_id_route(self, id):
         return db.get_user_by_id(db.id_creation(id))
