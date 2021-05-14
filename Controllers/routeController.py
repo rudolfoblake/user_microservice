53b646f3607a6e9f2a9e398085c362cc0c4133c8
Gustavo Lemos <gustavolemos7@hotmail.com>
2021-05-14 13:12:20 -0300

    Feat: User login route.

--- Controllers/routeController.py
+++ Controllers/routeController.py
@@ -26,6 +26,35 @@ class RouteControl:
         insert_user_in_database = db.create_user(user_data)
         return insert_user_in_database
 
+    def login_route(self, user_data: dict) -> tuple:
+        verify_user_login_requirements = ic.verify_user_login_requirements(user_data)
+        if verify_user_login_requirements[1] != 200:
+            return verify_user_login_requirements
+        if ac.password_is_encoded(user_data['password']):
+            decode_password = ac.password_decode(user_data['password'])
+            if decode_password == "":
+                return "Error: Failed to decode password!", 500
+            user_data['password'] = str(decode_password)
+        get_user_by_email = db.get_user_by_email(user_data["email"])
+        if get_user_by_email[1] == 200:
+            if not get_user_by_email[0]:
+                return "Error: A user with that email does not exists.", 400
+        else:
+            return "Error: Cannot execute search of user email in database.", 500
+        get_user_by_email = get_user_by_email[0]
+        try:
+            get_user_by_email['password']
+        except:
+            return "Error: Cannot get password from user on database to compare.", 500
+        if ac.password_is_encoded(get_user_by_email['password']):
+            decode_database_password = ac.password_decode(get_user_by_email['password'])
+            if decode_database_password == "":
+                return "Error: Failed to decode database password!", 500
+            get_user_by_email['password'] = decode_database_password
+        if get_user_by_email['password'] != user_data['password']:
+            return "Error: Invalid password!", 401
+        return get_user_by_email['_id']
+
 
     def get_user_by_id_route(self, id):
         return db.get_user_by_id(db.id_creation(id))
