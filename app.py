bf463b8959b527f249ea4437920e4e106fc69106
Gustavo Lemos <gustavolemos7@hotmail.com>
2021-05-17 08:17:29 -0300

    Feat: Task completed!

--- app.py
+++ app.py
@@ -20,6 +20,11 @@ def register_route():
 
 @app.route("/user/auth/login", methods=['POST'])
 def login_route():
+    """Rota de login de usuário
+    Verificar o json recebido e chamar o controller da rota de login.
+    Returns:
+        tuple(content, statuscode): Retorna o id do usuário conectado e o statuscode, em caso de erro retorna a mensagem de erro.
+    """	
     transfrom_to_dict = ic.json_to_dict(request)
     if transfrom_to_dict[1] != 200:
         return transfrom_to_dict
