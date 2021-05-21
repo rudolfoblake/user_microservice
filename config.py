5d83043f2e3385a17e3f4d3cecd4495fdcc7f9d2
Jefferson Roberto Bublitz Junior <67718795+bublitzjr@users.noreply.github.com>
2021-05-21 13:19:54 -0300

    Update config.py
    configurando port

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # CONFIG DB
 KEY = "WADCAlhXPqJ4UL3JwO4fkhsAV8rIyE"
 URL = "mongodb+srv://system:t7TRSmoJnO1DeZUa@cluster0.bawny.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-PORT = 3050
+PORT = 5030
 HOST = f"http://localhost:{PORT}"
 
 # CRYPT KEY
